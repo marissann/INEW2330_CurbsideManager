45cc1e8d535d6f5ccf5e4c6ca5fc7a2a8d675502
Scott Greenwood <70467130+Noax42@users.noreply.github.com>
2021-02-21 21:51:30 -0600

    Main Login Complete
    Added the .cs files for the most recent commit

---
+++ frmMain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMB3_Curbside_Manager
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //Check if the connection has not been established yet
+            if (!ProgOps.isConnected)
+            {
+                //Connect to the database
+                ProgOps.OpenDatabase();
+
+                //Tell ProgOps that the app is connected
+                ProgOps.isConnected = true;
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Disconnect to the database
+            ProgOps.CloseDatabase();
+        }
+        private void lblRecovery_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //Open the Login Recovery form
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //Check the database to see if the entered username and password match a customer or employee
+            ProgOps.LoginCommand(tbxUsername, tbxPassword);
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            //Open the Sign Up form
+        }
+    }
+}
